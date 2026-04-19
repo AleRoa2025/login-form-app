992b7cef699c4b5c9cbbb996cbeb90390c43d647
Alejandra Roa Peña <aroapena@gmail.com>
2026-04-19 05:04:34 -0400

    para publicar modifico json

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,18 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
 
 export default defineConfig({
+  base: "/login-form-app/",
+
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src')
-    }
- 
+      "@": resolve(__dirname, "src"),
+    },
   },
   test: {
-    environment: 'jsdom',
+    environment: "jsdom",
     globals: true,
-  }
-})
-export default {
-   base: '/https://github.com/AleRoa2025/login-form-app/', 
-}
+  },
+});
